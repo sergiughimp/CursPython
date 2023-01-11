cdce14f135047fd40aca896099cea988857a386f
Sergiu <ghimpsergiu@gmail.com>
2023-01-11 14:29:54 +0000

    'Redirect to Login page' Test

--- Curs12_POM/features/inventory.feature
+++ Curs12_POM/features/inventory.feature
@@ -5,3 +5,9 @@ Feature: Inventory Page
     Given I am on the Sign in page
     When I login successfully with "standard_user" and "secret_sauce"
     Then There are 6 products on the Inventory page
+  @single
+  Scenario: Redirect to Login page when unauthenticated user
+    Given I am not a log in user
+    When I go to the Inventory Page
+    Then I am redirected to Login Page
+    And I receive a Sad Face message
--- Curs12_POM/features/steps/inventory.py
+++ Curs12_POM/features/steps/inventory.py
@@ -7,3 +7,21 @@ def step_impl(context, username, password):
 @Then("There are 6 products on the Inventory page")
 def step_impl(context):
     assert context.inventory_page.get_products_count() == 6
+
+
+@Given('I am not a log in user')
+def step_impl(context):
+    pass
+
+@When('I go to the Inventory Page')
+def step_impl(context):
+    context.inventory_page.get_page()
+
+@Then('I am redirected to Login Page')
+def step_impl(context):
+    assert context.browser.get_current_url() == context.login_page.URL
+
+@Then('I receive a Sad Face message')
+def step_impl(context):
+    expected_err_msg = "Epic sadface: You can only access '/inventory.html' when you are logged in."
+    assert context.login_page.get_error_message() == expected_err_msg
